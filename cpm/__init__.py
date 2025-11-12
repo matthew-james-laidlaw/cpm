2074e12d9a1416fc7568d531540f47e089fcaa7b
Matthew Laidlaw <matthew.james.laidlaw@gmail.com>
2025-11-11 20:29:38 -0800

    can choose between debug and release, and also can call python as a cli tool like cpm --help rather than python main.py --help

--- cpm/__init__.py
+++ cpm/__init__.py
@@ -5,11 +5,11 @@ import subprocess
 from pathlib import Path
 
 def get_project_name_from_cache():
-    cache_file = Path("build") / "CMakeCache.txt"
+    cache_file = Path('build') / 'CMakeCache.txt'
     for line in cache_file.read_text().splitlines():
-        if line.startswith("CMAKE_PROJECT_NAME:STATIC="):
-            return line.split("=", 1)[1]
-    raise RuntimeError("Project name not found in CMakeCache.txt")
+        if line.startswith('CMAKE_PROJECT_NAME:STATIC='):
+            return line.split('=', 1)[1]
+    raise RuntimeError('could not find \'CMAKE_PROJECT_NAME\' in CMakeCache.txt')
 
 def handle_new(args):
     template_dir = Path(__file__).parent / 'templates' / 'main'
@@ -22,18 +22,19 @@ def handle_new(args):
     )
 
 def handle_build(args):
-    subprocess.run(['cmake', '-S','.', '-B', 'build', '-G', 'Ninja Multi-Config'], check=True)
-    subprocess.run(['cmake', '--build', 'build', '--config', 'Debug'], check=True)
+    config = 'Release' if args.release else 'Debug'
+    subprocess.run(['cmake', '-B', 'build', '-G', 'Ninja Multi-Config'], check=True)
+    subprocess.run(['cmake', '--build', 'build', '--config', f'{config}'], check=True)
 
 def handle_run(args):
-    app = str(Path('.') / 'build' / 'bin' / 'Debug' / get_project_name_from_cache())
-    subprocess.run([app])
+    config = 'Release' if args.release else 'Debug'
+    subprocess.run(['cmake', '--build', 'build', '--config', f'{config}', '--target', 'run'], check=True)
 
-if __name__ == '__main__':
+def main():
 
     parser = argparse.ArgumentParser(
         prog='cpm',
-        description='Manage C++ projects'
+        description='C++ Project Manager'
     )
 
     subparsers = parser.add_subparsers(
@@ -56,6 +57,7 @@ if __name__ == '__main__':
         description='Build this project'
     )
 
+    subcommand_build.add_argument('--release', help='Build with release flags', action='store_true')
     subcommand_build.set_defaults(func=handle_build)
 
     subcommand_run = subparsers.add_parser(
@@ -64,7 +66,11 @@ if __name__ == '__main__':
         description='Run this project'
     )
 
+    subcommand_run.add_argument('--release', help='Build with release flags', action='store_true')
     subcommand_run.set_defaults(func=handle_run)
 
     args = parser.parse_args()
     args.func(args)
+
+if __name__ == '__main__':
+    main()
